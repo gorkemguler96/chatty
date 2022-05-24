619c4ab22ac8060f60437bd99b80a059d03f65a1
gorkemguler96 <gorkemguler1996@gmail.com>
2022-05-24 18:54:43 +0300

    firebase işlemleri öğreniliyor

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBG0GiYc-uAcMWWKlUu8P-zQttIkZ87s8g",
+    authDomain: "chatty-4d4e3.firebaseapp.com",
+    databaseURL: "https://chatty-4d4e3-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chatty-4d4e3",
+    storageBucket: "chatty-4d4e3.appspot.com",
+    messagingSenderId: "617043085567",
+    appId: "1:617043085567:web:a507f2159997c7c58364ca"
+};
